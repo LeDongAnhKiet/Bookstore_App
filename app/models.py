2446b64685d3a6daea22d020165b1f89399d1673
Duong <1957012049duong@ou.edu.vn>
2022-11-23 23:12:16 +0700

    database 23/11

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class BaseModel(db.Model):
 
 
 class Category(BaseModel):
-    __tablename__ = 'category'
+    __tablename__ = 'Category'
 
     name = Column(String(50), nullable=False)
     products = relationship('Product', backref='category', lazy=True)
@@ -19,14 +19,43 @@ class Category(BaseModel):
         return self.name
 
 
-class Product(BaseModel):
-    __tablename__ = 'product'
+class Contributor(BaseModel):
+    __tablename__ = 'Contributor'
+    name = Column(String(50), nullable=False)
+    ContributorOfBook = relationship('ContributorOfBook', backref='contributor', lazy=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContributorOfBook(BaseModel):
+    __tablename__ = 'ContributorOfBook'
+    contributor_id = Column(Integer, ForeignKey(Contributor.id), nullable=False)
+    book_id = Column(Integer, ForeignKey(Book.id), nullable=False)
+    infocontributor_id = Column(Integer, ForeignKey(InfoContributor.id), nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(BaseModel):
+    __tablename__ = 'Book'
 
     name = Column(String(50), nullable=False)
     price = Column(Float, default=0)
     image = Column(String(100))
     quantity = Column(Integer)
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+    Contributorofbook_id = Column(Integer, ForeignKey(ContributorOfBook.id), nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class InfoContributor(BaseModel):
+    __tablename__ = 'InfoContributor'
+    name = Column(String(50), nullable=False)
+    contributor_id = Column(Integer, ForeignKey(Contributor.id), nullable=False)
 
     def __str__(self):
         return self.name
@@ -34,26 +63,26 @@ class Product(BaseModel):
 
 if __name__ == '__main__':
     with app.app_context():
-        # db.create_all()
-        # c1 = Category(name='Văn học')
-        # c2 = Category(name='Kinh tế')
-        # c3 = Category(name='Kỹ năng')
-        # c4 = Category(name='Sức khỏe')
-        # c5 = Category(name='Truyện trẻ em')
-        # c6 = Category(name='Lãng mạn')
-        # c7 = Category(name='Khoa học')
+        db.create_all()
+        # c1 = Category(name='Sách giáo khoa')
+        # c2 = Category(name='Ngoại ngữ')
+        # c3 = Category(name='Kiến thức - Khoa học')
+        # c4 = Category(name='Sách thiếu nhi')
+        # c5 = Category(name='Tâm lý - Kỹ năng sống')
+        # c6 = Category(name='Kinh tế')
+        # c7 = Category(name='Văn học - tiểu thuyết')
         #
         # db.session.add_all([c1, c2, c3, c4, c5, c6, c7])
         # db.session.commit()
 
-        p1 = Product(name='Thanh gươm do dự', price=199000,
-                     image='https://nhasachphuongnam.com/images/thumbnails/900/900/detailed/237/thanh-guom-do-du.jpg'
-                     , quantity=200, category_id=1)
-        p2 = Product(name='Lũ Trẻ Đường Tàu', price=129000,
-                     image='https://nhasachphuongnam.com/images/detailed/237/lu-tre-duong-tau.jpg'
-                     , quantity=200, category_id=2)
-        p3 = Product(name='Thanh gươm do dự', price=199000,
-                     image='https://nhasachphuongnam.com/images/thumbnails/900/900/detailed/237/thanh-guom-do-du.jpg'
-                     , quantity=200, category_id=3)
-        db.session.add_all([p1, p2, p3])
-        db.session.commit()
+        # p1 = Book(name='Thanh gươm do dự', price=199000,
+        #              image='https://nhasachphuongnam.com/images/thumbnails/900/900/detailed/237/thanh-guom-do-du.jpg'
+        #              , quantity=200, category_id=1)
+        # p2 = Book(name='Lũ Trẻ Đường Tàu', price=129000,
+        #              image='https://nhasachphuongnam.com/images/detailed/237/lu-tre-duong-tau.jpg'
+        #              , quantity=200, category_id=2)
+        # p3 = Book(name='Thanh gươm do dự', price=199000,
+        #              image='https://nhasachphuongnam.com/images/thumbnails/900/900/detailed/237/thanh-guom-do-du.jpg'
+        #              , quantity=200, category_id=3)
+        # db.session.add_all([p1, p2, p3])
+        # db.session.commit()
