16b87ba6276ae9ea786d308a8f646e810d512eef
unknown <205105368kiet@ou.edu.vn>
2022-12-06 01:18:54 +0700

    Chart Fixed

--- app/static/js/charts.js
+++ app/static/js/charts.js
@@ -1,47 +1,47 @@
-function CateStats(labels, data) {
+function drawCateStats (labels, data) {
     const ctx = document.getElementById('cateStats');
-    new Chart (ctx, {
-        type: 'pie',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Số lượng',
-                data: data,
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Số Lượng',
+        data: data,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
         }
-    });
+      }
+    }
+  });
 }
 
-function RevenueStats(labels, data) {
+function drawRevenueStats (labels, data) {
     const ctx = document.getElementById('revenueStats');
-    const rndNum = () => Math.floor(Math.random() * (255 - 50 + 1) + 50);
-    const rndRGBA = () => `rgba(${rndNum()}, ${rndNum()}, ${rndNum()}, 0.7)`;
-    new Chart (ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Doanh thu',
-                data: data,
-                borderWidth: 1
-                backgroundColor: ['red', 'blue', 'green', 'rndRGBA()', 'rndRGBA()']
-//                backgroundColor: ['red', 'blue', 'green', 'orange', 'rgba(100, 200, 150, 0.7)']
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+//    const rndNum = () => Math.floor(Math.random() * (255 - 55 + 1) + 55);
+//    const rndRGBA = () => `rgba(${rndNum()}, ${rndNum()}, ${rndNum()}, 1)`;
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Doanh Thu',
+        data: data,
+        borderWidth: 1,
+ backgroundColor: ['red', 'blue', 'green', 'rgba(100, 200, 150, 0.8)']
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
         }
-    });
+      }
+    }
+  });
 }
